9e05e0fe353e9e954edc97f4e1610679b96e19c3
--replace-all <nico7711@outlook.dk>
2020-07-01 22:45:59 +0200

    added validation function

--- app.js
+++ app.js
@@ -23,58 +23,39 @@ function addAdvert(){
     })
         
 }
-
+//INPUTS ----------------------------------------------------------------------------------
 function getInputs(){
     let adverts = [];
     document.querySelectorAll('.newAdvert').forEach(ad=>{
-        let allInputs = [];
-        ad.querySelectorAll('input').forEach(input=>{
-            allInputs.push(input.value)
-        })
-        if (
-            for (i=0; i<allInputs.length; i++){
-                allInputs[i].value = '';
-            }
-        ) {
-            
-        } else {
-            adverts.push({
-                price: parseInt(ad.querySelector('.advertPrice').value),
-                km:  parseInt(ad.querySelector('.advertKm').value),
-                years:  parseInt(ad.querySelector('.registeredDate').value),
-                optionals:  parseInt(ad.querySelector('.extraGear').value)
-            })
-        }
-         
-
-             
-
-        
-        
+       
+        adverts.push({
+            price: parseFloat(ad.querySelector('.advertPrice').value),
+            km:  parseFloat(ad.querySelector('.advertKm').value),
+            years:  parseFloat(ad.querySelector('.registeredDate').value),
+            optionals:  parseFloat(ad.querySelector('.extraGear').value)
+        })    
     })
 
-    let carStateSelect = document.querySelector('#vehicle-state-select');
-    let carStateOptions = carStateSelect.options[carStateSelect.selectedIndex].value;
+    let carConditionSelect = document.querySelector('#vehicle-state-select');
+    let carConditionOptions = parseFloat(carConditionSelect.options[carConditionSelect.selectedIndex].value);
+
     let specialUseSelect = document.getElementById('special-use-select');
-    let specialUseOptions = specialUseSelect.options[specialUseSelect.selectedIndex].value;
+    let specialUseOptions = parseFloat(specialUseSelect.options[specialUseSelect.selectedIndex].value);
 
     let inputs = {
         ads: adverts,
         myCarDate: document.getElementById('reg-dato').value,
-        myCarKm: parseInt(document.getElementById('total-km').value),
-        newPrice: parseInt(document.getElementById('nyprisberegner-1').value),
-        myCarOptionals: parseInt(document.getElementById('nyprisberegner-2').value),
-        carCondition: carStateOptions,
+        myCarKm: parseFloat(document.getElementById('total-km').value),
+        newPrice: parseFloat(document.getElementById('nyprisberegner-1').value),
+        myCarOptionals: parseFloat(document.getElementById('nyprisberegner-2').value),
+        carCondition: carConditionOptions,
         specialUse: specialUseOptions,
-        other: parseInt(document.getElementById('other-regulations').value)
+        other: parseFloat(document.getElementById('other-regulations').value)
     }
     return inputs;
 }
 
-
-
-
-
+// CALCULATIONS ------------------------------------------------------------
 function doCalculations(inputs){
     let data = inputs;
     
@@ -92,150 +73,190 @@ function doCalculations(inputs){
         let today = new Date().getTime();
 
         let age = today-milliseconds;
-        age = Math.floor(age / (1000*60*60*24*365));
+        age = Math.floor(age / (1000*60*60*24*365.25));
         return age;
     } 
     
     function averageSalesPrice() {
 
-        let priceFields = [];
-        data.ads.forEach(ad=>{
-            priceFields.push(ad.price)
-        })
-        let priceArray = [];
-        
-
-        priceFields.forEach(price=>{
-            
-            
-        
-            priceArray.push(parseInt(price.value));
-        
-        })
-
         let priceSum = 0;
 
-        priceArray.forEach(price=>{
-            priceSum += price
+        data.ads.forEach(ad=>{
+            priceSum+=ad.price
         })
 
-            
-        let priceAvg = Math.floor(priceSum/priceArray.length);
-        document.querySelector('.row-3_row-1_column-4_el-2').innerHTML = priceAvg + ' kr.'
-        console.log(priceFields)
+        let priceAvg = Math.floor(priceSum/data.ads.length);
+        
         return priceAvg;
     }
-    averageSalesPrice()
+   
     //Vognkort --------------
     function averageKilometers(){
-        let thisYear = new Date().getFullYear();
-        let myCarYear = new Date(document.getElementById('reg-dato').value).getFullYear();
-        let myCarAge = thisYear-myCarYear;
-        
-        let advertCarYear = document.querySelectorAll('.registeredDate');
-        let advertCarAge = [];
-        
-        advertCarYear.forEach(event => {
-            if (event.value == '') {
-                event.style.background = '#22aa22';
-            } else {
-                event.style.background = '#fff';
-                advertCarAge.push(thisYear-event.value)
-                //  console.log(thisYear-event.value)
-            }
-        })
-        
-        let kmArray = [];
-        let kmField = document.querySelectorAll('.advertKm');
-        
-        kmField.forEach(kmDriven=>{
-            if (kmDriven.value == '') {
-                kmDriven.style.background = '#22aa22';
-            } else {
-                kmArray.push(parseInt(kmDriven.value))
-            }
-        })
-        
-        let kmAvg = 0;
-        
-        for (i=0; i< kmField.length; i++) {
-            kmAvg += Math.floor((kmArray[i] / advertCarAge[i] * myCarAge) / kmField.length);
-        }
-        
-        console.log( kmAvg )
-        return kmAvg;
-        
-    }
+        let myCarAge = myCarYear();
 
+        myCarAge = (myCarAge === 0) ? 1: myCarAge;
+        let thisYear = new Date().getFullYear();
+
+        let kmDriven = [];
+        let adCarAge = [];
+
+        data.ads.forEach(ad=>{
+            kmDriven.push(ad.km)
+        })
+        data.ads.forEach(ad=>{
+            adCarAge.push(thisYear-ad.years)
+        })
+
+        let kmAvg = 0;
+
+        for (i=0; i< kmDriven.length; i++) {
+            kmAvg += (kmDriven[i] / adCarAge[i] * myCarAge) / kmDriven.length;
+        }        
+        
+        return kmAvg;
+    }
+ 
 
     function optionalsFormula(myOptionals, avgOptionals){
         let rates = [1, 0.7, 0.57, 0.45, 0.33, 0.20, 0.15, 0.10, 0];
-        let age = myCarAge();
+        let age = myCarYearMonth();
         age = (age < 8) ? age : 8;
         
-        return Math.floor(
-            (myOptionals-avgOptionals)*rates[age]
-            );
-        }
+        return (myOptionals-avgOptionals)*rates[age];
+    }
         
-        function avgOptionalsCalc(){
-        let optionalsInput = document.querySelectorAll('.extraGear');
+    function avgOptionalsCalc(){
         let optionalsSum = 0;
         
-        optionalsInput.forEach(input=>{
-            if (input.value == ''){
-                input.style.background = '#22aa22';
-            } else {
-                input.style.background = '#fff';
-                optionalsSum += parseInt(input.value);
-            }
+        data.ads.forEach(ad=>{
+            optionalsSum += ad.optionals;
         })
         
-        let optionalsAvg = Math.floor(optionalsSum / optionalsInput.length)
-        //console.log(optionalsAvg);
+        let optionalsAvg = optionalsSum / data.ads.length;
+       
         return optionalsAvg;
     }
 
     function optionalsCalc() {
-        let myOptionals = parseInt(document.getElementById('nyprisberegner-2').value);
+        let myOptionals = data.myCarOptionals;
         let avgOptionals = avgOptionalsCalc();
         
         let optionalsDepreciated = Math.floor(optionalsFormula(myOptionals, avgOptionals));
         
-        document.querySelector('.row-3_row-2_row-2_el-2').innerHTML = optionalsDepreciated + ' kr.';
-        console.log(optionalsDepreciated)
         return optionalsDepreciated;
+    }
+
+    // Most price calculation is done under here
+    function fivePercentDeduction(){
+        let deduction = averageSalesPrice()*-0.05;
+        return deduction;
     }
 
     function deductedPrice(){
         let marketPrice = averageSalesPrice();
+        let fivePercent = fivePercentDeduction();
+        const constant = -2000;
         
-        
-        function fivePercent(){
-            let deduction = Math.floor(marketPrice*0.05);
-            document.querySelector('.row-3_row-1_column-3_el-2').innerHTML = deduction + ' kr.';
-            return deduction;
-        }
-        console.log(fivePercent())
-        const constant = 2000;
-        
-        let afterDeductions = Math.floor(marketPrice - (fivePercent()+constant));
-        document.querySelector('.row-3_row-1_column-4_el-3').innerHTML = afterDeductions + ' kr.';
-        
+        let afterDeductions = marketPrice + fivePercent+constant;
         return afterDeductions;
     }
 
+
+
+    function carCondition(marketPrice, percent){
+        let result = marketPrice * percent;
+        return (result > -20000) ? result : -20000;
+    }
+
+    function specialUse(marketPrice, percent) {
+        let result = marketPrice*percent;
+        return (result > -20000) ? result : -20000;
+       
+    }
+
+    function conditionAndUse(){
+        let marketPrice = averageSalesPrice();
+        let conditionPercent = data.carCondition;
+        let specialUsePercent = data.specialUse;
+
+        let conditionAmount = carCondition(marketPrice, conditionPercent);
+        let specialUseAmount = specialUse(marketPrice, specialUsePercent);
+        
+        let result = conditionAmount + specialUseAmount;
+   
+        return (result > -20000) ? result : -20000;
+    }
+
+    function kmRegulation(){
+        
+    }
+
+    function finalPrice(){
+        let price = deductedPrice()+optionalsCalc()+conditionAndUse();
+        return price;
+    }
+
+    finalPrice()
+
+    let outputs = {
+        finalPrice: finalPrice(),
+        avgPrice:  averageSalesPrice(),
+        optionals: optionalsCalc(),
+        deductedPrice: deductedPrice(),
+        fivePercent: fivePercentDeduction(),
+        conditionAndUse: conditionAndUse()
+
+    }
+    return outputs;
 }
 
 
+// OUTPUTS -----------------------------------------------------------------------------------
+function outputs(results){
+    let output = results;
 
+    document.querySelector('.row-3_row-1_column-4_el-2').innerHTML = Math.floor(output.avgPrice) + ' kr.';
+
+    document.querySelector('.row-3_row-2_row-2_el-2').innerHTML = Math.floor(output.optionals) + ' kr.';
+
+    document.querySelector('.row-3_row-1_column-4_el-3').innerHTML = Math.floor(output.deductedPrice) + ' kr.';
+
+    document.querySelector('.row-3_row-1_column-3_el-2').innerHTML = Math.floor(output.fivePercent) + ' kr.';
+    document.querySelector('.row-3_row-2_row-5_el-2').innerHTML = Math.floor(output.finalPrice) + ' kr.'
+
+    document.querySelector('.row-3_row-2_row-1_el-2').innerHTML = Math.floor(output.conditionAndUse) + ' kr.'
+
+
+}
+
+// Validates the calculator; checks if all inputs are filled in -------------------------
+function validateForm() {
+    let isValid = true;
+
+    let inputs = document.querySelectorAll('input');
+
+    inputs.forEach(function(e){
+        if (e.value == '') {
+            isValid = false;
+            e.style.background = 'red';
+        } else {
+            e.style.background = null;
+        }
+    })
+
+
+    return isValid;
+}
+
+// triggers everything ----------------------------------------------
 document.addEventListener('keyup', event=>{
     if (event.keyCode === 13) {
-        var inputs = getInputs();
-        doCalculations(inputs)
-        
+       // if(validateForm()) {
+
+            var inputs = getInputs();
+            let calc = doCalculations(inputs)
+            outputs(calc);
+      //  }
 
     }
 })
-
-
